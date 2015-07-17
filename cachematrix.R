bd782f2a4eaf1a63550cfa3b78cc278ae0efde92
jamistry <jamistry@usc.edu>
2015-07-16 19:34:34 -0700

    PA2
    Update for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+
 ## the functions check to make sure the inputted matrix is invertible, then create it, then check to see if the inverse has been cached, and if not calculate the inverse
-## Write a short comment describing this function
-## checks if the matrix can be inverted, then makes it along with the inverse placeholder
+
+## checks if the matrix can be inverted, then makes the invertible matrix object along with defining the different functions that will be used on the object, also defines the placeholder for the inverse value
 makeCacheMatrix <- function(x = matrix()) {
  
-if(nrow(x)==ncol(x)){  
-inverse<<- NULL
- x<<-x
-  return(x)
+
+  inverse <- NULL
+get <- function(){
+  x
 }
-else{
-  return("Matrix must be square to be invertible")
+set <- function(y){
+  x<<-y
+  inverse <<- NULL
 }
+
+getinverse <- function(){
+  inverse
 }
+setinverse <- function(inv){
+  inverse <<- inv
+}
+list(get = get, set = set, getinverse = getinverse, setinverse = setinverse)
+}
+
+
 
 
 ## Write a short comment describing this function
-## checks if inverse has been cached, if not it calculates, caches, and returns it
+## checks if inverse has been cached, if not it calculates, caches, and returns it. If it's been cached and the matrix hasn't changed, returns the cached inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  if(!is.null(inverse) && inverse == solve(x)){
-    print("retrieving cached data")
+  inverse <- x$getinverse()
+  if(!is.null(inverse)){
+    print("Retrieving cached data")
     return(inverse)
   }
-  inverse<<- solve(x)
+  data <- x$get()
+  inverse <- solve(data)
+  x$setinverse(inverse)
   return(inverse)
+
 }
